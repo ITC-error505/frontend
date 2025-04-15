185810f4e1355ab2fb85c8e93b342980687b53d6
bearismybrother <thetwai123456789@gmail.com>
2025-04-15 13:48:40 -0700

    Linked Signup Page

--- SignUp.js
+++ SignUp.js
@@ -1,6 +1,6 @@
 document
   .getElementById('signupForm')
-  .addEventListener('submit', function (event) {
+  .addEventListener('submit', async function (event) {
     event.preventDefault();
 
     // Get values from input fields
@@ -12,23 +12,48 @@ document
       alert('Passwords do not match!');
       return;
     }
-    const response = registerAccount(username, password);
+    const response = await registerAccount(username, password);
 
     //This part is strange.
     if (response.ok) {
-      window.location.href = './SignIn.html';
+      const logInResponse = await logInAccount(username, password);
+      if (logInResponse.ok) {
+        const userId = await logInResponse.json();
+        localStorage.setItem('userId', userId);
+        window.location.href = './Games.html';
+      } else {
+        alert('Something went wrong with logging in');
+        window.location.href = './SignIn.html';
+      }
     } else {
       alert('Something went wrong with registration');
     }
   });
 
 async function registerAccount(username, password) {
-  const response = await fetch('http://localhost:32775/account/register', {
-    method: 'POST',
-    body: JSON.stringify({ username: username, password: password }),
-    headers: {
-      'Content-Type': 'application/json', // <-- Important!
-    },
-  });
+  const response = await fetch(
+    'https://backend-aqzm.onrender.com/account/register',
+    {
+      method: 'POST',
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+        'Content-Type': 'application/json', // <-- Important!
+      },
+    }
+  );
+  return response;
+}
+
+async function logInAccount(username, password) {
+  const response = await fetch(
+    'https://backend-aqzm.onrender.com/account/login',
+    {
+      method: 'POST',
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+        'Content-Type': 'application/json', // <-- Important!
+      },
+    }
+  );
   return response;
 }
