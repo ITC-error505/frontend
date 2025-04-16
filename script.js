5d39d4eaf068a20261adfabac3d51eb15ebd66c6
Tavina Chen <tavinachen.work@gmail.com>
2025-04-15 17:04:56 -0700

    working leaderboards not yet connected to database

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
 
+
+
+let testArray = [{'rank':'1', 'username':'Bob', 'score':'1234'}, 
+                {'rank':'2', 'username':'John', 'score':'124'}]
+
+                let ArrTwo = [{'rank':'1', 'username':'John', 'score':'1234'}, 
+                {'rank':'2', 'username':'Bob', 'score':'124'}]
+
+                let Arr3 = [{'rank':'2', 'username':'John', 'score':'1234'}, 
+                {'rank':'2', 'username':'Bobby', 'score':'124'}]
+
+let currentData = testArray
+
+populateBoard(currentData)
+
+/**Switch Tabs and update board */
+const tabs = document.querySelectorAll(".tab")
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
+        /**Switch tabs Active*/
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
         })
-        tabs.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        })
-        target.classList.add('active')
         tab.classList.add('active')
+
+        /**Update board to correct game*/
+        if (tab.classList.contains("warrior")) {
+            currentData = ArrTwo
+        } else if (tab.classList.contains("popper")) {
+            currentData = Arr3
+        } else {
+            currentData = testArray
+        }
+
+        populateBoard(currentData)
     })
 })
+
+/**Populate table*/
+function populateBoard(data) {
+    var table = document.getElementById('table')
+    table.innerHTML = ""
+    
+    for (var i = 0; i < data.length; i++) {
+        if (i >= 10) {
+            break;
+        }
+
+        var row = "<tr><td>" + data[i].rank +
+                    "</td><td>" +
+                    data[i].username+ 
+                    "</td><td>" +
+                    data[i].score+
+                    "</td></tr>"
+
+        table.innerHTML += row;
+    }
+}
+
+
+   
