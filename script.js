6d21bad6e5944d127e4c43cccaf2300824508a7d
bearismybrother <thetwai123456789@gmail.com>
2025-04-15 23:21:44 -0700

    Fixed Leaderboard, now shows proper rows from database

--- script.js
+++ script.js
@@ -10,94 +10,83 @@
 //let currentData = testArray
 
 /**Load starting data */
-let currentData = []
+let currentData = [];
 var currentGame = 1;
-updateBoard(currentGame)
+updateBoard(currentGame);
 
 /**Switch Tabs and update board */
-const tabs = document.querySelectorAll(".tab")
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        /**Switch tabs Active*/
-        tabs.forEach(tab => {
-            tab.classList.remove('active')
-        })
-        tab.classList.add('active')
+const tabs = document.querySelectorAll('.tab');
+tabs.forEach((tab) => {
+  tab.addEventListener('click', async () => {
+    /**Switch tabs Active*/
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    tab.classList.add('active');
 
-        /**Update board to correct game*/
-        if (tab.classList.contains("warrior")) {
-            currentGame = 1
-            //currentData = testArray
-        } else if (tab.classList.contains("popper")) {
-            currentGame = 2
-            //currentData = ArrTwo
-        } else {
-            currentGame = 3
-            //currentData = Arr3
-        }
-        updateBoard(currentGame)
-    })
-})
+    /**Update board to correct game*/
+    if (tab.classList.contains('warrior')) {
+      currentGame = 1;
+      //currentData = testArray
+    } else if (tab.classList.contains('popper')) {
+      currentGame = 2;
+      //currentData = ArrTwo
+    } else {
+      currentGame = 3;
+      //currentData = Arr3
+    }
+    await updateBoard(currentGame);
+  });
+});
 
 /**Update Board Values*/
 
-function updateBoard(gameType) {
-    if (gameType == 1) {
-        //currentData = testArray
+async function updateBoard(gameType) {
+  var response;
+  if (gameType == 1) {
+    //currentData = testArray
+    response = await fetchLeaderBoard(2);
+  } else if (gameType == 2) {
+    //currentData = ArrTwo
 
-        $.ajax( {
-            method: 'GET',
-            url: 'https://backend-aqzm.onrender.com/score/leaderboard?gameId=1',
-            success:function(response) {
-                currentData = response.data
-                populateBoard(currentData)
-            }
-        })
-    } else if (gameType == 2) {
-        //currentData = ArrTwo
-
-        $.ajax( {
-            method: 'GET',
-            url: 'https://backend-aqzm.onrender.com/score/leaderboard?gameId=2',
-            success:function(response) {
-                currentData = response.data
-                populateBoard(currentData)
-            }
-        })
-    } else {
-        //currentData = Arr3
-        $.ajax( {
-            method: 'GET',
-            url: 'https://backend-aqzm.onrender.com/score/leaderboard?gameId=3',
-            success:function(response) {
-                currentData = response.data
-                populateBoard(currentData)
-            }
-        })
-    }
-
-    populateBoard()
+    response = await fetchLeaderBoard();
+  } else {
+    //currentData = Arr3
+    response = await fetchLeaderBoard(1);
+  }
+  currentData = await response.json();
+  populateBoard();
 }
 
+async function fetchLeaderBoard(gameId) {
+  const response = await fetch(
+    'https://backend-aqzm.onrender.com/score/leaderboard?gameId=' + gameId,
+    {
+      method: 'GET',
+    }
+  );
+  return response;
+}
 
 /**Populate table*/
 function populateBoard() {
-    var table = document.getElementById('table')
-    table.innerHTML = ""
-    
-    for (var i = 0; i < currentData.length; i++) {
-        if (i >= 10) {
-            break;
-        }
+  var table = document.getElementById('table');
+  table.innerHTML = '';
 
-        var row = "<tr><td>" + currentData[i].rank +
-                    "</td><td>" +
-                    currentData[i].username+ 
-                    "</td><td>" +
-                    currentData[i].score+
-                    "</td></tr>"
-
-        table.innerHTML += row;
+  for (var i = 0; i < currentData.length; i++) {
+    if (i >= 10) {
+      break;
     }
+
+    var row =
+      '<tr><td>' +
+      currentData[i].rank +
+      '</td><td>' +
+      currentData[i].username +
+      '</td><td>' +
+      currentData[i].score +
+      '</td></tr>';
+
+    table.innerHTML += row;
+  }
 }
-   
