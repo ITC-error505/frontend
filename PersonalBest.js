700e44375bb8b601314d13df39b5b8a977744430
Tavina Chen <tavinachen.work@gmail.com>
2025-04-16 12:19:20 -0700

    working personal best

---
+++ PersonalBest.js
@@ -0,0 +1,42 @@
+
+/**Loading screens */
+loading('warrior');
+loading('popper');
+loading('man');
+
+function loading(id) {
+    var game = document.getElementById(id);
+    game.innerHTML = '<div>Rank: Loading...</div><div>Score: Loading...</div>';
+
+}
+
+
+var userID = 1;
+
+/**get user ranks and high scores */
+updateScores(2, 'warrior');
+updateScores(3, 'popper');
+updateScores(1, 'man');
+
+async function updateScores(gameId, id) {
+    var game = document.getElementById(id);
+
+    var response = await getRanking(gameId);
+    var data = await response.json();
+
+    game.innerHTML = '<div>Rank: ' +
+                    data.rank + 
+                    '</div><div>Score: ' +
+                    data.highScore +
+                    ' pts </div>'
+}
+
+async function getRanking(gameId) {
+    const response = await fetch(
+      `https://backend-aqzm.onrender.com/score/highScore?accountId=${userID}&gameId=${gameId}`,
+      {
+        method: 'GET',
+      }
+    );
+    return response;
+  }
