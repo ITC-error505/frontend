d3be5059ac7e061399b426a33db4ef2f612a72da
bearismybrother <thetwai123456789@gmail.com>
2025-04-24 12:24:29 -0700

    Made web tokens check for expiration date

--- Authorization.js
+++ Authorization.js
@@ -1,3 +1,19 @@
-if (!localStorage.getItem('userIdTokenized')) {
-  window.location.href = './SignIn.html';
+token = localStorage.getItem('userIdTokenized');
+
+if (token) {
+  validateTokenExpiration(token).then((response) => {
+    if (!response.ok) {
+      window.location.href = './SignIn.html';
+    }
+  });
+}
+
+async function validateTokenExpiration(token) {
+  const response = await fetch(
+    `https://backend-aqzm.onrender.com/account/validateTokenExpiration?token=${token}`,
+    {
+      method: 'POST',
+    }
+  );
+  return response;
 }
